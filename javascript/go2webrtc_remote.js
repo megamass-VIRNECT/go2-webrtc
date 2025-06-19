77c7f0b8b176fcec7ea0324d578f7e8a2f4fe6da
megamass <megamass@virnect.com>
2025-06-19 16:17:12 +0900

    수정 0619-2

--- javascript/go2webrtc_remote.js
+++ javascript/go2webrtc_remote.js
@@ -73,7 +73,7 @@ export class Go2WebRTC {
     console.log(config);
     this.pc = new RTCPeerConnection({
       sdpSemantics: "unified-plan",
-      iceTransportPolicy: "relay",
+      // iceTransportPolicy: "relay",
       ...config
     });
 
