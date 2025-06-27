005a12a5c7343f79ebbe66c69df71463fcafd63c
megamass <megamass@virnect.com>
2025-06-27 14:42:24 +0900

    0627-16

--- javascript/go2webrtc_remote.js
+++ javascript/go2webrtc_remote.js
@@ -115,9 +115,9 @@ export class Go2WebRTC {
 
     console.log(config);
     this.pc = new RTCPeerConnection({
-      sdpSemantics: "unified-plan"
+      sdpSemantics: "unified-plan",
       // iceTransportPolicy: "relay",
-      // ...config
+      ...config
     });
 
     this.channel = this.pc.createDataChannel("data");
